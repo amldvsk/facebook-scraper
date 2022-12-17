e3d3628b79ef89bfc6eaeb82d984692f7e804297
adi <amldvsk@gmail.com>
2022-12-17 16:41:16 +0200

    Update constants.py

--- facebook_scraper/constants.py
+++ facebook_scraper/constants.py
@@ -1,6 +1,6 @@
 FB_BASE_URL = 'https://facebook.com/'
 FB_W3_BASE_URL = 'https://www.facebook.com/'
-FB_MOBILE_BASE_URL = 'https://m.facebook.com/'
+FB_MOBILE_BASE_URL = 'https://mbasic.facebook.com/'
 FB_MBASIC_BASE_URL = 'https://mbasic.facebook.com/'
 
 DEFAULT_REQUESTS_TIMEOUT = 30
